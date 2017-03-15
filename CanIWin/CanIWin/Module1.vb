d771eb26ad83ed61b1dfa5a1c751688c777ed0a6
Conrad Lim <conradp04@yahoo.com>
2017-03-14 21:19:59 -0700

    Add files via upload
    Initial Code

---
+++ CanIWin/CanIWin/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.Write("Enter Amount of Stones:")
+        Dim n As Integer = Console.ReadLine()
+        Console.WriteLine(n)
+
+        Console.WriteLine("Make a move between 1 to 3:")
+        Console.Write("Choose your move:")
+        Dim m As Integer = Console.ReadLine()
+        Dim answer As Integer = n Mod m
+
+        Console.WriteLine("answer: ")
+        Console.WriteLine(answer)
+
+
+        If answer = 0 Then
+            Console.WriteLine("You win")
+        ElseIf answer > 0 AndAlso answer <= 3 Then
+            Console.WriteLine("You lose")
+
+        End If
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
