b948986c3664220b5ef707be97031aaea24ec876
Conrad Lim <conradp04@yahoo.com>
2017-03-20 22:29:14 -0700

    Add files via upload

--- CanIWin/CanIWin/Module1.vb
+++ CanIWin/CanIWin/Module1.vb
@@ -1,27 +1,47 @@
 ﻿Module Module1
 
     Sub Main()
-        Console.Write("Enter Amount of Stones:")
+        Console.WriteLine("    ******************************    ")
+        Console.WriteLine("    Finding the Max, Min and Average  ")
+        Console.WriteLine("    ******************************    ")
+        Console.WriteLine()
+        Console.Write("How many numbers would you like to enter? ")
         Dim n As Integer = Console.ReadLine()
-        Console.WriteLine(n)
+        Dim x As Integer = 1
+        Dim minNum As Double = 2000000
+        Dim maxNum As Double = 0
+        Dim total As Double = 0.00
+        Console.WriteLine("     ")
+        While x <= n
+            Console.Write("Choice number " + x.ToString() + ": ")
+            Dim xNum As Double = Console.ReadLine()
 
-        Console.WriteLine("Make a move between 1 to 3:")
-        Console.Write("Choose your move:")
-        Dim m As Integer = Console.ReadLine()
-        Dim answer As Integer = n Mod m
 
-        Console.WriteLine("answer: ")
-        Console.WriteLine(answer)
+            If xNum > maxNum Then
+                maxNum = xNum
+            End If
+            If xNum < minNum Then
+                minNum = xNum
+            End If
+            total += xNum
+            x += 1
 
+        End While
+        Console.WriteLine()
+        Console.WriteLine("Max Number is: " + maxNum.ToString())
+        Console.WriteLine("Min Number is: " + minNum.ToString())
+        Console.WriteLine("Avg Number is: " + (total / n).ToString())
+        Console.WriteLine()
+        Console.WriteLine("    ******************************    ")
+        Console.WriteLine("Want to play again? Y/N")
+        Dim answer As String = Console.ReadLine()
 
-        If answer = 0 Then
-            Console.WriteLine("You win")
-        ElseIf answer > 0 AndAlso answer <= 3 Then
-            Console.WriteLine("You lose")
+        If answer.ToUpper() = "Y" Then
+            Main()
+        Else
 
+            Environment.Exit(0)
         End If
-
-
         Console.ReadKey()
 
     End Sub
